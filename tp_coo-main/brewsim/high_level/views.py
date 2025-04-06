7d8c202ab26d9959a0a789fbdd8fafe8b2fc58ff
aouaKanazoe <aouak2025@gmail.com>
2025-04-06 21:05:02 +0200

    Initial commit

---
+++ tp_coo-main/brewsim/high_level/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView
+from .models import Departement
+from .models import Machine
+from .models import Ingredient
+from .models import QuantiteIngredient
+from .models import Action
+from .models import Recette,Usine,Prix
+from django.http import HttpResponse
+from json import dumps
+
+# Create your views here.
+
+class DepartementDetailView(DetailView):
+    model = Departement
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(dumps(self.object.json()))
+
+class MachineDetailView(DetailView):
+    model = Machine
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(dumps(self.object.json()))
+
+class IngredientDetailView(DetailView):
+    model = Ingredient
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(dumps(self.object.json()))
+
+class QuantiteIngredientDetailView(DetailView):
+    model = QuantiteIngredient
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(dumps(self.object.json()))
+
+class ActionDetailView(DetailView):
+    model = Action
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(dumps(self.object.json()))
+
+class RecetteDetailView(DetailView):
+    model = Recette
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(dumps(self.object.json()))
+class UsineDetailView(DetailView):
+    model = Usine
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(dumps(self.object.json()))
+class PrixDetailView(DetailView):
+    model = Prix
+    def render_to_response(self, context, **response_kwargs):
+        return HttpResponse(dumps(self.object.json()))

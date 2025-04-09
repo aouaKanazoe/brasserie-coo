54ee3441e1cada20be0b870309ee4cb90bdbe4af
aouaKanazoe <aouak2025@gmail.com>
2025-04-09 18:21:20 +0200

    nouveaunew commit

--- brasserie/config/urls.py
+++ brasserie/config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from high_level.views import DepartementDetailView
+from brewsim.high_level.views import DepartementDetailView
 
 urlpatterns = [
     path("admin/", admin.site.urls),

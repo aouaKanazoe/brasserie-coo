6e8758d6648ae3bfc8ea4ce5d65c613069d581b3
aouaKanazoe <aouak2025@gmail.com>
2025-04-14 18:44:27 +0200

    sixteenth commit

--- brasserie/config/settings.py
+++ brasserie/config/settings.py
@@ -79,7 +79,9 @@ USE_TZ = True
 # Static files configuration
 STATIC_URL = "/static/"
 
-#STATICFILES_DIRS = []
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static")
+ ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # pour collectstatic
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
